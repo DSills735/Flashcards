54a78b6dcb241d45482f4d94c5583a3bd5b6edcc
DSills735 <David.sills100@gmail.com>
2026-02-11 14:32:27 -0600

    Add project files.

---
+++ Flashcards/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sql;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    internal static IConfiguration config = new ConfigurationBuilder()
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+    static string? connectionString = config.GetConnectionString("DefaultConnection");
+
+
+
+}
