cbdb0a964302baf35ab8039f782808cb006f92af
DSills735 <David.sills100@gmail.com>
2026-03-05 10:09:20 -0600

    added todo note.

--- Flashcards/Mapping/FlashcardMapper.cs
+++ Flashcards/Mapping/FlashcardMapper.cs
@@ -3,7 +3,7 @@ namespace Flashcards.Mapping
 {
     public static class FlashcardMapper
     {
-
+        //TODO figure out why this isnt working
         public static DTO.FlashcardDTO ToDto(this Flashcard flaschard)
         {
             return new DTO.FlashcardDTO(
