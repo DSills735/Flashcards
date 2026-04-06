d2b0ac952fcb4b49b109e2a2e8f8a69ea8d249ca
DSills735 <David.sills100@gmail.com>
2026-04-06 08:03:23 -0500

    updated search by ID method

--- Flashcards/Card_Ops/DeleteFlashcard.cs
+++ Flashcards/Card_Ops/DeleteFlashcard.cs
@@ -17,24 +17,21 @@ namespace Flashcards.Card_Ops
 
             Database_Helpers.ViewFlashcards.FlashcardsToTable();
             bool exists = false;
-            
-            string resp = Console.ReadLine()!;
 
-            while (!exists || resp == null)
-            {
-                int count = connection.ExecuteScalar<int>(SQL_Helpers.SqlHelper.SearchFlashcardsByID(), new { FlashcardID = resp });
+            var resp = AnsiConsole.Prompt(
+    new TextPrompt<int>("Enter the [green]Flashcard ID[/]:")
+        .InvalidChoiceMessage("[red]That's not a valid ID format![/]")
+        .Validate(id =>
+        {
+            int count = connection.ExecuteScalar<int>(
+                SQL_Helpers.SqlHelper.SearchFlashcardsByID(),
+                new { FlashcardID = id }
+            );
 
-                if (count == 0)
-                {
-
-                    AnsiConsole.MarkupLine(@"[rapidblink][maroon]ERROR!![/][/][red] This stack was not found.[/] Please ensure the ID you are entering is reflected on the table above.");
-                    resp = Console.ReadLine()!;
-                }
-                else
-                {
-                    exists = true;
-                }
-            }
+            return count > 0
+                ? ValidationResult.Success()
+                : ValidationResult.Error("[maroon]ERROR!![/] This stack was not found.");
+        }));
 
             AnsiConsole.MarkupLine("[rapidblink][maroon] Are you sure you want to delete this flashcard?[/][/] Y / N");
 
